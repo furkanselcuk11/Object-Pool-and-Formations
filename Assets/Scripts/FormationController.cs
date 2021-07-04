e9f07afb2200ecef707b1dcc1e6bdd8c42d31bea
Furkan Selçuk <64227907+furkanselcuk11@users.noreply.github.com>
2021-07-04 15:34:19 +0300

    +add: CircleFormation added

---
+++ Assets/Scripts/FormationController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormationController : MonoBehaviour
+{
+    public GameObject prefabObj;
+
+    public int newObjSize = 10;    // Eklenecek prefabObj sayýsý
+    public float distanceBetweenObj = 1.5f;    // Yeni eklenecek nesneler arasý uzaklýk mesafesi 
+    void Start()
+    {
+        CircleFormation();
+    }
+
+    private void CircleFormation()
+    {
+        Vector3 targetPos = Vector3.zero;   // Hedef nesnenin pozisyonunu 0 yapar
+
+        for (int i = 0; i < newObjSize; i++)
+        {
+            GameObject newObj = Instantiate(prefabObj); // Oluþturulacak yeni nesneler
+
+            float angle = i * (2 * 3.14159f / newObjSize);    // Açý tanýmlanýr - Eklenecek nesne sayýsýna bölünür
+            float x = Mathf.Cos(angle) * distanceBetweenObj;  // Cos açýsý(angle) 
+            float y = Mathf.Sin(angle) * distanceBetweenObj;  // Sin açýsý(angle)  
+
+            targetPos = new Vector3(targetPos.x+x,targetPos.y+y,0); // Oluþturulacak olan yeni nesnenin pozisyonlarý belirlenir
+            newObj.transform.position = targetPos;  // "newObj" adlý yeni oluþturulacak nesnenin pozisyonu "targetPos"un vector deðerlerini alýr
+        }
+    }
+}
